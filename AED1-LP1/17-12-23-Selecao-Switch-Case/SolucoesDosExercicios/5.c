96aea5ae72b906f4442eecb2aeed3728775b5eb9
Pedro Dias <63060088+pedrexds@users.noreply.github.com>
2023-12-12 10:27:32 -0400

    Create 5.c

---
+++ AED1-LP1/17-12-23-Selecao-Switch-Case/SolucoesDosExercicios/5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    float altura, pesoIdeal=0;
+    char genero;
+    
+    printf("Digite a altura da pessoa (em cm): ");
+    scanf("%f", &altura);
+    fflush(stdin);
+    printf("Digite o gênero da pessoa:\n(M) para Masculino\n(F) para Feminino\n");
+    scanf("%c", &genero);
+    
+    switch(genero)
+    {
+        case 'M': pesoIdeal = 52+(0.75*(altura - 152.4));
+                  break;
+        case 'F': pesoIdeal = 52+(0.67*(altura - 152.4));
+                  break;
+        default: printf("Opção Inválida.");
+    }
+    
+    if(pesoIdeal != 0)
+    {
+        printf("Peso ideal calculado: %.2f kg.", pesoIdeal);
+    }
+    
+    return 0;
+}
