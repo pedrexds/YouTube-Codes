b02eb9bf5efed24af465e74bfbbecf3fe05432cb
Pedro Dias <63060088+pedrexds@users.noreply.github.com>
2023-12-20 13:40:38 -0400

    Create 2.c

---
+++ AED1-LP1/07-01-24-While-EstruturaDeRepeticao/SolucoesDosExercicios/2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int totalAlunos, contador = 1;
+    float nota, soma=0.0;
+
+    printf("Número de alunos na sala: ");
+    scanf("%i",&totalAlunos);
+
+    while(contador <= totalAlunos)
+    {
+        printf("Nota do %iº aluno: ", contador);
+        scanf("%f",&nota);
+
+        soma = soma + nota;
+
+        contador
+        ++;
+    }
+
+    printf("Media da turma: %.2f", soma/totalAlunos);
+    
+    return 0;   
+}
